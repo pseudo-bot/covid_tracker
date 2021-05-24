d2d504ec9508d9c44a2fa08280560f2cd282cba1
pseudobot <shivamx6174@gmail.com>
2021-05-25 03:42:08 +0530

    search system

--- main.js
+++ main.js
@@ -1,42 +1,49 @@
 function menu() {
-    const menubar = document.querySelector('.menubar');
-    const sideMenu = document.querySelector('.navbar__items');
+  const menubar = document.querySelector(".menubar");
+  const sideMenu = document.querySelector(".navbar__items");
 
-    menubar.addEventListener('click', () => {
-        menubar.classList.toggle('menubar--activate');
-        sideMenu.classList.toggle('navbar__items--activate')
-    });
+  menubar.addEventListener("click", () => {
+    menubar.classList.toggle("menubar--activate");
+    sideMenu.classList.toggle("navbar__items--activate");
+  });
 }
 
 menu();
 
-
-const statesUrl = 'states.json';
-const statesList = document.querySelector('#statesList');
-const stateBtn = document.querySelector('.states_btn');
-const districtsList = document.querySelector('#districtsList');
-const stateInput = document.querySelector('#states_input');
+const statesUrl = "states.json";
+const statesList = document.querySelector("#statesList");
+const stateBtn = document.querySelector(".states_btn");
+const districtsList = document.querySelector("#districtsList");
+const stateInput = document.querySelector("#states_input");
 
 fetch(statesUrl)
-    .then(res => res.json())
-    .then(data => {
-            for (let state of data['states']) {
-                let stateNode = document.createElement(`option`);
-                stateNode.setAttribute('value', `${state.state}`);
-                statesList.appendChild(stateNode);
-            }
+  .then((res) => res.json())
+  .then((data) => {
+    for (let state of data["states"]) {
+      let stateNode = document.createElement(`option`);
+      stateNode.setAttribute("value", `${state.state}`);
+      statesList.appendChild(stateNode);
+    }
 
-            stateBtn.addEventListener('click', () => {
-                enterValue();
-                console.log(stateInput.value);
-                // for (let state of data['states']) {
-                //     for (let district of state.districts) {
-                //         let districtsNode = document.createElement(`option`);
-                //         districtsNode.setAttribute('value', `${district}`);
-                //         districtsList.appendChild(districtsNode);
-                //     }
-                // }
-            })
-        }
-    )
+    stateBtn.addEventListener("click", () => {
+      removeAllNodes(districtsList);
 
+      if (!stateInput.value) return;
+
+      const stateIndex = data["states"].findIndex(
+        (element) => element.state === stateInput.value
+      );
+
+      for (let district of data["states"][stateIndex].districts) {
+        let districtNode = document.createElement("option");
+        districtNode.setAttribute("value", `${district}`);
+        districtsList.appendChild(districtNode);
+      }
+    });
+  });
+
+function removeAllNodes(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+}
