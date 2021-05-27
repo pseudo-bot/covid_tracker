c3aa27707ab35acaaadae4916b59cb41b2b9ec06
pseudobot <shivamx6174@gmail.com>
2021-05-27 18:22:30 +0530

    themchange-white

--- dump.js
+++ dump.js
@@ -1,3 +1,3 @@
-fetch('https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=AIzaSyBcsCr8jrWKO1pSWrC4QDRuK9lt2C7Y-Y0')
-.then(resolve => resolve.json())
-.then(data => console.log(data))
+let date = new Date();
+
+console.log(date)
--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 function menu() {
 	const menubar = document.querySelector(".menubar");
 	const sideMenu = document.querySelector(".navbar__items");
@@ -11,35 +10,54 @@ function menu() {
 
 menu();
 
+
+// Animations 
+
+document.querySelector('.heading').addEventListener('click', () => {
+	document.querySelector('.heading').classList.add('.animateOnLoad')
+})
+
 const country = document.querySelector("#country");
-const inputCountry = document.querySelector('.input--country')
-const inputState = document.querySelector('.input--states')
+const inputCountry = document.querySelector(".input--country");
+const inputState = document.querySelector(".input--states");
+const totalCases = document.querySelector(".total_cases");
 
-
-async function data() {
-	const response = await fetch(
-		"https://api.covid19api.com/summary"
-	);
+async function data_() {
+	const response = await fetch("https://api.covid19api.com/summary");
 	const values = await response.json();
 	return values;
 }
 
-(async function populateCountry() {
-	const dataC = await data();
+// Search functions
 
-	for (let countryVar of dataC.Countries) {
+async function populateCountry() {
+	const data = await data_();
+
+	for (let countryVar of data.Countries) {
 		const countryNode = document.createElement("OPTION");
 		countryNode.setAttribute("value", countryVar.Country);
 		country.appendChild(countryNode);
 	}
-})();
+}
 
-inputState.addEventListener('click', () => {
-	if (inputCountry.value.toLowerCase() === 'india') {
-		inputState.removeAttribute('readonly');
+inputState.addEventListener("click", () => {
+	if (inputCountry.value.toLowerCase() === "india") {
+		inputState.removeAttribute("readonly");
 	} else {
-		inputState.value = '';
-		inputState.setAttribute('readonly', '');
+		inputState.value = "";
+		inputState.setAttribute("readonly", "");
 	}
-})
+});
 
+async function populateGlobal() {
+	const data = await data_();
+
+	
+}
+
+const execute = () => {
+	populateCountry();
+	populateGlobal();
+};
+
+execute();
